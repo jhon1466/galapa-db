a9aa3ebd3f9b1e0d8c5c258b7de0109738e22eef
jhon1466 <164232093+jhon1466@users.noreply.github.com>
2026-04-03 16:39:27 -0500

    Auto-sync: update manifest for appid 2527500 via Proxy

---
+++ manifests/2527500.lua
@@ -0,0 +1,12 @@
+-- 2527500's Lua and Manifest Created by Morrenus
+-- MiSide
+-- Created: September 30, 2025 at 01:08:56 EDT
+-- Website: https://manifest.morrenus.xyz/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(2527500) -- MiSide
+-- MAIN APP DEPOTS
+addappid(2527501, 1, "ffef2d1f42cd2438f5b7d029d771a9f941fd224250c3184f595773fb4926e1cd") -- Depot 2527501
+setManifestid(2527501, "5992599505980992417", 4251911821)
