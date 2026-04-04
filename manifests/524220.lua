d9780e89b97028b6e320d212c00f555aa054a66d
jhon1466 <164232093+jhon1466@users.noreply.github.com>
2026-04-03 23:40:33 -0500

    Auto-sync: update manifest for appid 524220 via Proxy

---
+++ manifests/524220.lua
@@ -0,0 +1,50 @@
+-- 524220's Lua and Manifest Created by Morrenus
+-- NieR:Automata™
+-- Created: September 30, 2025 at 03:31:17 EDT
+-- Website: https://manifest.morrenus.xyz/
+-- Total Depots: 10
+-- Total DLCs: 8
+-- Shared Depots: 1
+
+-- MAIN APPLICATION
+addappid(524220) -- NieR:Automata™
+-- MAIN APP DEPOTS
+addappid(524221, 1, "710eaf838fca522739fd6f0741fc539dbfb3e69df0bca4f0daed6d9789cac571") -- NieR:Automata™ Default
+setManifestid(524221, "4633942970953135000", 43318634773)
+-- SHARED DEPOTS (from other apps)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") -- DirectX Jun 2010 Redist (Shared from App 228980)
+setManifestid(228990, "1829726630299308803", 102931551)
+-- DLCS WITH DEDICATED DEPOTS
+-- NieRAutomata - 3C3C1D119440927 (AppID: 580600)
+addappid(580600)
+addappid(580600, 1, "67169947379fca33e6a1fd52fd5d4c6f45234151a71bfd9f5e6cda8bdcfab349") -- NieRAutomata - 3C3C1D119440927 - NieR:Automata™ - 3C3C1D119440927 (580600) 個のデポ
+setManifestid(580600, "6352626735786606959", 3507768459)
+-- NieRAutomata - Grimoire Weiss Pod (AppID: 580610)
+addappid(580610)
+addappid(580610, 1, "b6542835e2fd44948c08a553cab57020dce3620f6677ff161f908160be1de0dd") -- NieRAutomata - Grimoire Weiss Pod - NieR:Automata™ - Grimoire Weiss Pod (580610) 個のデポ
+setManifestid(580610, "70539499618618565", 2920624)
+-- NieRAutomata - Machine Mask Accessory (AppID: 583990)
+addappid(583990)
+addappid(583990, 1, "cfe4dbbe0d5622ff101a177b2bd7e089e1fb1afd241cda936d0fc3ac7d4010d1") -- NieRAutomata - Machine Mask Accessory - NieR:Automata™ - Mask Character Accessory (583990) 個のデポ
+setManifestid(583990, "7857600567806192582", 659632)
+-- NieRAutomata - Valve Character Accessory (AppID: 583991)
+addappid(583991)
+addappid(583991, 1, "6d4ffe017d1c3b9a876f16684204feb1f303a3b6f8327a8cd5246c74799587f0") -- NieRAutomata - Valve Character Accessory - NieR:Automata™ - Valve Character Accessory (583991) 個のデポ
+setManifestid(583991, "7822405453448242645", 727288)
+-- NieRAutomata - Cardboard Pod Skin (AppID: 583992)
+addappid(583992)
+addappid(583992, 1, "8f903ba92f0e6eb40ae27b57eb1a1ba3672599687013ec0a1685bffb8cf78a93") -- NieRAutomata - Cardboard Pod Skin - NieR:Automata™ - Cardboard Pod Skin (583992) 個のデポ
+setManifestid(583992, "7955785643780797608", 4163760)
+-- NieRAutomata - Retro Red Pod Skin (AppID: 583993)
+addappid(583993)
+addappid(583993, 1, "8feaf96bd01fb95f5a288544933692972ad398cf76355a6589a6c85475f7cc95") -- NieRAutomata - Retro Red Pod Skin - NieR:Automata™ - Retro Red Pod Skin (583993) 個のデポ
+setManifestid(583993, "3440919711813183915", 4688048)
+-- NieRAutomata - Retro Grey Pod Skin (AppID: 583994)
+addappid(583994)
+addappid(583994, 1, "8b754108a01dcdb855841b04bdac5882b22d07844e6d364adbe5efd46d8ecdaa") -- NieRAutomata - Retro Grey Pod Skin - NieR:Automata™ - Retro Grey Pod Skin (583994) 個のデポ
+setManifestid(583994, "6837545457254028528", 4688048)
+-- NieRAutomata - Game of the YoRHa Edition Wallpaper Set (AppID: 1017430)
+addappid(1017430)
+addtoken(1017430, "1808551213462367460")
+addappid(1017430, 1, "3f639279ce3e6f19d6e4093f0af4032334e31a0e1203288a5da6419f82d04720") -- NieRAutomata - Game of the YoRHa Edition Wallpaper Set - NieR:Automata™ - Game of the YoRHa Edition Wallpaper Set (1017430) デポ
+setManifestid(1017430, "1820157406263693639", 16090077)
