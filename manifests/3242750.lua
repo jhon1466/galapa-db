c91c4d4d6740e2cb27c59ed457792fb272bc6bca
jhon1466 <164232093+jhon1466@users.noreply.github.com>
2026-04-03 22:10:41 -0500

    Auto-sync: update manifest for appid 3242750 via Proxy

---
+++ manifests/3242750.lua
@@ -0,0 +1,10 @@
+-- Made by f1uxin, enjoy!
+-- Redistribution/sharing of these files is allowed but you must keep everything as is. 
+-- Feel free to join my(F1uxins) official server: https://discord.gg/f1uxins-ship
+
+-- MAIN APPLICATION
+addappid(3242750) -- Level Devil
+
+-- MAIN APP DEPOTS
+addappid(3242751, 1, "20715373400230b7184afdfb64c33c5a97093edd019c92ada23c16db734de7b3") -- Main Game Content (Windows Content)
+setManifestid(3242751, "8294518347077805506", 0)
