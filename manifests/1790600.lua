0434b28539a33a8db39152a1b8204626cb0b7a2b
jhon1466 <164232093+jhon1466@users.noreply.github.com>
2026-04-05 14:13:50 -0500

    Auto-sync: update manifest for appid 1790600 via Proxy

---
+++ manifests/1790600.lua
@@ -0,0 +1,42 @@
+-- 1790600's Lua and Manifest Created by Morrenus
+-- DRAGON BALL: Sparking! ZERO
+-- Created: February 06, 2026 at 06:58:25 EST
+-- Website: https://manifest.morrenus.xyz/
+-- Total Depots: 5
+-- Total DLCs: 13
+-- Shared Depots: 2
+
+-- MAIN APPLICATION
+addappid(1790600, 1, "5c8b5e47bb9da92fe0a31a0d56554bb62851cb9f846cc5f89fac68501d9964ff") -- DRAGON BALL: Sparking! ZERO
+addtoken(1790600, "4807984869702339794")
+-- MAIN APP DEPOTS
+addappid(1790601, 1, "0721a07eb97ba7b4e59e31606ae9f0d67c66309cc467c5da1aa79a924c34e7d8") -- Depot 1790601
+setManifestid(1790601, "7463464431421101707", 30636861270)
+-- SHARED DEPOTS (from other apps)
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") -- VC 2022 Redist (Shared from App 228980)
+setManifestid(228989, "3514306556860204959", 39590283)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") -- DirectX Jun 2010 Redist (Shared from App 228980)
+setManifestid(228990, "1829726630299308803", 102931551)
+-- DLCS WITH DEDICATED DEPOTS
+-- DRAGON BALL Sparking ZERO Anime Music Pack 1 (AppID: 2957480)
+addappid(2957480)
+addappid(2957480, 1, "a8bbd2bdc3022cc2edc229e300b7583436b29d0e469662a8a277cf066798aa9f") -- DRAGON BALL Sparking ZERO Anime Music Pack 1 - Depot 2957480
+setManifestid(2957480, "2926878599274088682", 25771532)
+-- DRAGON BALL Sparking ZERO Anime Music Pack 2 (AppID: 2957490)
+addappid(2957490)
+addappid(2957490, 1, "1b1f85ab25eb92147de8a0c1531d9ed57f3e01cfd46c9bb37be377251c2d24cd") -- DRAGON BALL Sparking ZERO Anime Music Pack 2 - Depot 2957490
+setManifestid(2957490, "6336205044388274470", 41112630)
+-- DLCS WITHOUT DEDICATED DEPOTS
+addappid(2957460) -- DRAGON BALL Sparking ZERO Pre-Order Pack
+addappid(2957470) -- DRAGON BALL Sparking ZERO Ultimate Upgrade Pack
+addappid(2957500) -- DRAGON BALL Sparking ZERO Victory Pack
+addappid(2957510) -- DRAGON BALL Sparking ZERO Martial Arts Pack
+addappid(2957520) -- DRAGON BALL Sparking ZERO Season Pass
+addappid(2963440) -- DRAGON BALL Sparking ZERO Ultimate Edition Bonus
+addappid(2963450) -- DRAGON BALL Sparking ZERO Season Pass Bonus
+addappid(3310650) -- DRAGON BALL Sparking ZERO HERO OF JUSTICE Pack
+addappid(3456600) -- DRAGON BALL Sparking ZERO - Dragon Ball DAIMA Character Pack 1
+addappid(3456610) -- DRAGON BALL Sparking ZERO - Dragon Ball DAIMA Character Pack 2
+addappid(3693250) -- DRAGON BALL Sparking ZERO - Shallot (DRAGON BALL LEGENDS)
+-- EMPTY DEPOTS (no content on any branch)
+-- addappid(1790602) -- Depot 1790602 (empty depot)
