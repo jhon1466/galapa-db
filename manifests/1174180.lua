0ef356108b8b858311bc17f96be1ac8f5b961ebd
jhon1466 <164232093+jhon1466@users.noreply.github.com>
2026-04-08 17:10:13 -0500

    Auto-sync: update manifest for appid 1174180 via Proxy

---
+++ manifests/1174180.lua
@@ -0,0 +1,35 @@
+-- 1174180's Lua and Manifest Created by Morrenus
+-- Red Dead Redemption 2
+-- Created: March 31, 2026 at 18:22:33 EDT
+-- Website: https://manifest.morrenus.xyz/
+-- Total Depots: 9
+-- Total DLCs: 2
+-- Shared Depots: 3
+
+-- MAIN APPLICATION
+addappid(1174180, 1, "c4ba69ff66208b16fc27679ceefb89d490776fc18bae801311a31b6855b74783") -- Red Dead Redemption 2
+-- MAIN APP DEPOTS
+addappid(1174181, 1, "6fbf0f4b33fd5111d0832f0fb63a4d185c5321901ef95776c73e6e10b203bc25") -- Red Dead Redemption 2 Install
+setManifestid(1174181, "4757128409362785099", 746)
+addappid(1174182, 1, "0824e4e00b9d464a1352b0eaa60416ee3bfe0f3b416459991dc6fca87e84366f") -- Red Dead Redemption 2 Base
+setManifestid(1174182, "2258488483491377476", 78804578319)
+addappid(1174183, 1, "cd51ee7e8adde797736bc283f9d434fae0aac28753e3f38a29a78aac298c0ccf") -- Red Dead Redemption 2 Redist
+setManifestid(1174183, "4785346550216140531", 390640)
+addappid(1174184, 1, "506c8f971b77f5c39f8baa54fac090e79bf32e19d92eca2a34cba9569bbe5d8a") -- Red Dead Redemption 2 Update
+setManifestid(1174184, "2698939236196220127", 5460348964)
+addappid(1174185, 1, "6fe9c5b4e41f1f91b6a775dd562515e9652570cc607caedef2bc4a9d522fea4d") -- Red Dead Redemption 2 Packs
+setManifestid(1174185, "8173342615013177810", 17767404187)
+addappid(1174186, 1, "0ef7331ad866994474a72131eeb1a5667dc8c9f7be87b4e1d805f288e1d49432") -- Red Dead Redemption 2 Audio
+setManifestid(1174186, "357926288062992323", 26009156930)
+-- SHARED DEPOTS (from other apps)
+addappid(228987, 1, "cf0622b6dec67606fdc42e7afa5ede78cb33e38dfab82670c5ec7e1404e4984e") -- VC 2017 Redist (Shared from App 228980)
+setManifestid(228987, "4302102680580581867", 29664201)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") -- DirectX Jun 2010 Redist (Shared from App 228980)
+setManifestid(228990, "1829726630299308803", 102931551)
+addappid(1899671, 1, "b7921da5e50d00b2238d0fe870a354cb572bc5d397955fef02a439103f62827b") -- RGL/SC Content (Shared from App 1899670)
+setManifestid(1899671, "274155245002712969", 239649837)
+-- DLCS WITHOUT DEDICATED DEPOTS
+addappid(1190050) -- Red Dead Redemption 2 Special Edition Content
+addtoken(1190050, "4679005205774328491")
+addappid(1190051) -- Red Dead Redemption 2 Ultimate Edition Content
+addtoken(1190051, "14772589015440947562")
