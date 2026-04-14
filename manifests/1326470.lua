a4ad26a65ddf56718f37ae234c3e43eafa938c8e
jhon1466 <164232093+jhon1466@users.noreply.github.com>
2026-04-13 20:22:59 -0500

    Auto-sync: update manifest for appid 1326470 via Proxy

--- manifests/1326470.lua
+++ manifests/1326470.lua
@@ -1,3 +1,12 @@
-addappid(1326470)
-addappid(1326471,1,"dcc8b76a7563c836c1aed54a46abf85e7fec5fccabc0dd8e176d2151173e508e")
-setManifestid(1326471,"8162016777027728069",0)
+-- 1326470's Lua and Manifest Created by Morrenus
+-- Sons Of The Forest
+-- Created: October 03, 2025 at 17:15:54 EDT
+-- Website: https://manifest.morrenus.xyz/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(1326470) -- Sons Of The Forest
+-- MAIN APP DEPOTS
+addappid(1326471, 1, "dcc8b76a7563c836c1aed54a46abf85e7fec5fccabc0dd8e176d2151173e508e") -- Depot 1326471
+setManifestid(1326471, "8162016777027728069", 15989503975)
