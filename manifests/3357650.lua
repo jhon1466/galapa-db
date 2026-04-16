f697508e35248e128b67d3184e2e9f385469874f
jhon1466 <164232093+jhon1466@users.noreply.github.com>
2026-04-15 21:36:04 -0500

    Auto-sync: update manifest for appid 3357650 via Proxy

---
+++ manifests/3357650.lua
@@ -0,0 +1,19 @@
+-- 3357650's Lua and Manifest Created by Hubcap Manifest
+-- PRAGMATA
+-- Created: April 15, 2026 at 13:44:12 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 2
+-- Total DLCs: 0
+-- Shared Depots: 1
+
+-- MAIN APPLICATION
+addappid(3357650, 1, "18e75bfb3f8c26b7a59d9e3a7013e74e1c59fe563aa00ee15f54a35b17691766") -- PRAGMATA
+-- MAIN APP DEPOTS
+addappid(3357651, 1, "18e75bfb3f8c26b7a59d9e3a7013e74e1c59fe563aa00ee15f54a35b17691766") -- Depot 3357651
+setManifestid(3357651, "2417499809052404547", 36816287661)
+-- SHARED DEPOTS (from other apps)
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") -- VC 2022 Redist (Shared from App 228980)
+setManifestid(228989, "3514306556860204959", 39590283)
+-- EMPTY DEPOTS (no content on any branch)
+-- addappid(3357652) -- Depot 3357652 (empty depot)
+-- addappid(4093670) -- Depot 4093670 (empty depot)
