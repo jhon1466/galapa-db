cbdf8fefee9c81fba8923743c5ee90a5ad974c38
jhon1466 <164232093+jhon1466@users.noreply.github.com>
2026-04-24 10:18:26 -0500

    Auto-sync: update manifest for appid 880940 via Proxy

--- manifests/880940.lua
+++ manifests/880940.lua
@@ -1,4 +1,9 @@
+-- Made by f1uxin, please read the (README) file and enjoy!
+-- P.S. If there is no README file, this file has most likely been stolen the creator of these files is @𓆩𝙁𝟙𝙪𝙭𝙞𝙣𓆪 / username - f1uxin. On discord, nobody else.
+-- If you want to redistribute/share these files, read the README TXT for more info.
+-- Feel free to join my (F1uxins) official server: https://discord.gg/NmmpgnAgen
+-- (We show everything about piracy, including manifest & lua stuff also known as the "Steam method", all for free, Nobody should need to pay for info that is spread and shown for free.)
+
 addappid(880940, 1, "5279674a32bc137d15796bc284c97323c365f4aabc5c5781cd337906f34367a1")
 addappid(880941,0,"ce9aa368e895b19d481941c1bc33d93fa1b495f6046caeee282580a6eac1a99c")
-setManifestid(880941,"8651477077702693582")
-addappid(880940,0,"5279674a32bc137d15796bc284c97323c365f4aabc5c5781cd337906f34367a1")
+setManifestid(880941,"201500148968686526")
